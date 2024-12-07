f541600f7f0301b7fefaafb165d8182a1a9a59b9
Mohasin <mohasin61@gmail.com>
2024-12-07 22:54:14 +0530

    add cors

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import type { Application } from "express";
+import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 
 export const initMiddlewares = (app: Application) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
+  app.use(cors());
   app.use(morgan("dev"));
   app.use(helmet());
 };
